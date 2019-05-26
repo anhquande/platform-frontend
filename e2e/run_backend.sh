a130aafca6009f6e9c8f48c3918e8df02aeb9b31
Marcin Rudolf <marcin.rudolf@neufund.org>
2019-05-26 09:14:12 +0200

    increases block mining time on local backend instance

--- e2e/run_backend.sh
+++ e2e/run_backend.sh
@@ -58,7 +58,7 @@ make docker-pull tag=dev_${BACKEND_SHORT_SHA}
 echo "Pulling images done."
 
 make prerequisites-dev
-make run-without-build blocks_freq=0.2 request_timeout=60
+make run-without-build blocks_freq=0.5 request_timeout=60
 
 cd ..
 echo "Backend running"
